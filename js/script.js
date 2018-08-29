847fa989a66b7bef86f516a03b92b79e5445a959
Justin Velez <justin.velez0523@gmail.com>
2018-08-29 00:58:31 +0000

    Added code to call NYT API for descriptions/headlines

--- js/script.js
+++ js/script.js
@@ -24,3 +24,15 @@ function loadData() {
 };
 
 $('#form-container').submit(loadData);
+
+//NyTime Ajax Request
+var nytimesURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q='+ cityStry + "&sort=newest&api-key=1b55d9eb5b4b4481a44f287fafb28db4"
+$.$.getJSON("ntyimesURL", function(data) {
+    $nytHeaderElem.text('New York Times Articles About'+ cityStr);
+    articles = data.response.docs;
+    for (var i=0; i < articles.length; i++) {
+        var articles = articles [i];
+        $nytElem.append ('<li class="article> '+
+    '<a href="' + article.web_url + '">' + article.headline.main + '</a>' + '<p>'+ article.snippet + '</p>'+'</li>');
+    };
+} );
